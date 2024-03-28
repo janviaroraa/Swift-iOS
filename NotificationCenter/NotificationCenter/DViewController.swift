2b0c52885d51b4779eb674ddddc1d2178db2472b
Janvi Arora <jjanvi.arora18@gmail.com>
2024-03-28 22:53:43 +0530

    Notification Center implementation

---
+++ NotificationCenter/NotificationCenter/DViewController.swift
@@ -0,0 +1,46 @@
+//
+//  DViewController.swift
+//  Protocol-Delegate
+//
+//  Created by Janvi Arora on 28/03/24.
+//
+
+import UIKit
+
+class DViewController: UIViewController {
+
+    private lazy var button: UIButton = {
+        let btn = UIButton()
+        btn.translatesAutoresizingMaskIntoConstraints = false
+        btn.setTitle("Send Data", for: .normal)
+        btn.backgroundColor = .systemBlue
+        btn.layer.cornerRadius = 4
+        btn.setTitleColor(.white, for: .normal)
+        btn.addTarget(self, action: #selector(navigate), for: .touchUpInside)
+        return btn
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        addViews()
+        layoutConstraints()
+    }
+
+    private func addViews() {
+        view.addSubview(button)
+    }
+
+    private func layoutConstraints() {
+        NSLayoutConstraint.activate([
+            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            button.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            button.heightAnchor.constraint(equalToConstant: 30),
+        ])
+    }
+
+    @objc private func navigate() {
+        NotificationCenter.default.post(name: Notification.Name("sendData"), object: nil, userInfo: ["data": "Janvi-Adarsh"])
+        navigationController?.popToRootViewController(animated: true)
+    }
+}
